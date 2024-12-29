2e9342fb599e9d9f069222b19177be6b95e5fb00
JustOrangeDrink <sadcat2005@gmail.com>
2024-12-29 13:46:04 +0300

    Added horizontal looping, buttons to add and kill generation and range inputs to configure heights and width of the screen.

--- Scripts/simulation.js
+++ Scripts/simulation.js
@@ -1,28 +1,33 @@
 import { gridToWorld, worldToGrid, randomMinMax } from "./utilities.mjs";
 
-const screen_width = 100;
-const screen_height = 100;
+const screen_width = document.getElementsByClassName("width_input")[0].value;
+const screen_height = document.getElementsByClassName("height_input")[0].value;
 
-const renderingSpeed = document.getElementsByClassName(
+const tile = 6;
+
+const renderingSpeedInput = document.getElementsByClassName(
   "rendering_speed_input"
-)[0].value;
+)[0];
 
 const cellsAmount =
   document.getElementsByClassName("cells_amount_input")[0].value;
 
+const spawnButton = document.getElementsByClassName("spawn_button")[0];
+const killButton = document.getElementsByClassName("kill_button")[0];
+
 const WIDTH =
-  6 *
+  tile *
   Math.floor(
     ((document.getElementsByClassName("simulation")[0].offsetWidth / 100) *
       screen_width) /
-      6
+      tile
   );
 const HEIGHT =
-  6 *
+  tile *
   Math.floor(
     ((document.getElementsByClassName("simulation")[0].offsetHeight / 100) *
       screen_height) /
-      6
+      tile
   );
 console.log("Width: " + WIDTH);
 console.log("Height: " + HEIGHT);
@@ -49,15 +54,30 @@ for (let x = 0; x < WIDTH_GRID; x++) {
     grid.push(tile);
   }
 }
+console.log(grid);
 
+renderLife();
 function renderLife() {
   for (let i = 0; i < grid.length; i++) {
     if (grid[i].state == "alive") {
-      context.fillStyle = "#33ccaa";
-      context.fillRect(gridToWorld(grid[i].x), gridToWorld(grid[i].y), 6, 6);
+      let colorR = randomMinMax(0, 150);
+      let colorG = randomMinMax(215, 255);
+      let colorB = randomMinMax(215, 255);
+      context.fillStyle = `rgb(${colorR}, ${colorG}, ${colorB})`;
+      context.fillRect(
+        gridToWorld(grid[i].x),
+        gridToWorld(grid[i].y),
+        tile,
+        tile
+      );
     } else {
       context.fillStyle = "#000000";
-      context.fillRect(gridToWorld(grid[i].x), gridToWorld(grid[i].y), 6, 6);
+      context.fillRect(
+        gridToWorld(grid[i].x),
+        gridToWorld(grid[i].y),
+        tile,
+        tile
+      );
     }
   }
 }
@@ -70,18 +90,19 @@ function countNeighbors() {
   for (let i = 0; i < grid.length; i++) {
     if (grid[i].state == "alive") {
       let neighbors = getNeighborsIndex(i);
-      for (let neighborIndex in neighbors) {
-        if (grid[neighbors[neighborIndex]] == undefined) continue;
-        grid[neighbors[neighborIndex]].aliveNeighbors++;
+      for (let neighbor in neighbors) {
+        let index = neighbors[neighbor];
+        if (index < 0) {
+          index = grid.length + index;
+        }
+        if (index > grid.length - 1) {
+          index = index - grid.length;
+        }
+        if (!grid[index]) {
+          throw new Error("Index isn't in grid!");
+        }
+        grid[index].aliveNeighbors++;
       }
-    }
-  }
-}
-
-function getGridIndexByCoordinates(x, y) {
-  for (let i = 0; i < grid.length; i++) {
-    if (grid[i].x == x && grid[i].y == y) {
-      return i;
     }
   }
 }
@@ -100,29 +121,42 @@ function getNeighborsIndex(core) {
   return directions;
 }
 
-console.log(grid);
+function spawnGeneration(cellsAmount) {
+  for (let i = 0; i < cellsAmount; i++) {
+    let randomIndex = randomMinMax(0, grid.length - 1);
+    grid[randomIndex].state = "alive";
+  }
+  renderLife();
+}
 
-renderLife();
+function killGeneration() {
+  for (let i = 0; i < grid.length; i++) {
+    grid[i].state = "dead";
+  }
+}
 
 function iterateLife() {
   countNeighbors();
   for (let i = 0; i < grid.length; i++) {
-    if (grid[i].aliveNeighbors == 3) {
+    let tile = grid[i];
+    let neighborsAmount = tile.aliveNeighbors;
+    if (neighborsAmount == 3) {
       grid[i].state = "alive";
     }
-    if (grid[i].aliveNeighbors < 2 || grid[i].aliveNeighbors > 3) {
+    if (neighborsAmount < 2 || neighborsAmount > 3) {
       grid[i].state = "dead";
     }
   }
   renderLife();
 }
-
-for (let i = 0; i < cellsAmount; i++) {
-  let randomIndex = randomMinMax(1, WIDTH_GRID * HEIGHT_GRID);
-  if (grid[randomIndex] == undefined) continue;
-  grid[randomIndex].state = "alive";
-}
-
 setInterval(() => {
   iterateLife();
-}, renderingSpeed);
+}, renderingSpeedInput.value);
+
+spawnButton.onclick = function () {
+  spawnGeneration(cellsAmount);
+  renderLife();
+};
+killButton.onclick = function () {
+  killGeneration();
+};
